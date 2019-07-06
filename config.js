b4323f9251d543a5a94d6005da592ac012cce0a0
thedrew177 <52588436+thedrew177@users.noreply.github.com>
2019-07-05 21:55:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ module.exports = {
     'prefix': '*',
     'ticketParentChannel': '469251649471250432',
     'ticketCategory': '469251649471250432',
+    'viewTickets': ['519350702686994465'],
     'submissionChannels': ['593634365213245450', '582678435420438531', '486048422256967681'],
     'reactionEmotes': ['👍', '👎'],
 }
