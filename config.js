473b839444b755bae86c4cbfe891cc7ff403dd7d
thedrew177 <52588436+thedrew177@users.noreply.github.com>
2019-07-05 21:48:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'token': '',
     'nickname': 'Professor Drew',
     'activity': 'Primal Moon',
-    'prefix': '!',
+    'prefix': '.',
     'ticketParentChannel': '469251649471250432',
     'ticketCategory': '469251649471250432',
     'submissionChannels': ['593634365213245450', '582678435420438531', '486048422256967681'],
